b2d1e54001e35a46ebe2456bc7b69c7bd6375c98
Roman <roman141aleksandro2007@gmail.com>
2026-05-07 23:01:18 +0300

    CDate исправлен, добавлены нужные ограничения в методы через add_day() add_month() add_year()

--- GeometryLib/geometry_main.cpp
+++ GeometryLib/geometry_main.cpp
@@ -9,39 +9,58 @@ int geometry_main() {
     std::cin >> circle1.center.x;
     std::cin >> circle1.center.y;
     std::cin >> circle1.r;
-    std::cout << "Your first circle: " << "( " << circle1.center.x << ", " << circle1.center.y << ") " << circle1.r << "\n";
+    std::cout << "Your first circle: " << "(" << circle1.center.x << ", " << circle1.center.y << ") " << circle1.r << "\n";
 
     std::cout << "Input second circle: ";
     std::cin >> circle2.center.x;
     std::cin >> circle2.center.y;
     std::cin >> circle2.r;
-    std::cout << "Your second circle: " << "( " << circle2.center.x << ", " << circle2.center.y << ") " << circle2.r << "\n";
+    std::cout << "Your second circle: " << "(" << circle2.center.x << ", " << circle2.center.y << ") " << circle2.r << "\n";
     system("cls");
-
+    std::cout << "\n" << "Your first circle: " << "(" << circle1.center.x << ", " << circle1.center.y << ") " << circle1.r << "\n";
+    std::cout << "Your second circle: " << "(" << circle2.center.x << ", " << circle2.center.y << ") " << circle2.r << "\n";
     switch (get_position(circle1, circle2)) {
     case 0:
-        std::cout << "Not-related - не пересекаются.";
+        std::cout << "╔══════════════════════════════════╗\n";
+        std::cout << "║  Not-related - не пересекаются.  ║\n";
+        std::cout << "╚══════════════════════════════════╝\n";
         break;
+
     case 1:
-        std::cout << "Intersection - пересекаются. ";
+        std::cout << "╔════════════════════════════════╗\n";
+        std::cout << "║  Intersection - пересекаются.  ║\n";
+        std::cout << "╚════════════════════════════════╝\n";
         break;
+
     case 2:
-        std::cout << "Equal - совпадают. ";
+        std::cout << "╔══════════════════════╗\n";
+        std::cout << "║  Equal - совпадают.  ║\n";
+        std::cout << "╚══════════════════════╝\n";
         break;
+
     case 3:
-        std::cout << "Touch - касаются. ";
+        std::cout << "╔═════════════════════╗\n";
+        std::cout << "║  Touch - касаются.  ║\n";
+        std::cout << "╚═════════════════════╝\n";
         break;
+
     case 4:
-        std::cout << "One inside the another - одна внутри другой. ";
+        std::cout << "╔════════════════════════════════════════════════╗\n";
+        std::cout << "║  One inside the another - одна внутри другой.  ║\n";
+        std::cout << "╚════════════════════════════════════════════════╝\n";
         break;
+
     case 5:
-        std::cout << "NONE - error. ";
+        std::cout << "╔═════════════════╗\n";
+        std::cout << "║  NONE - error.  ║\n";
+        std::cout << "╚═════════════════╝\n";
         break;
+
     default:
-        std::cout << "No response - error. ";
+        std::cout << "╔════════════════════════╗\n";
+        std::cout << "║  No response - error.  ║\n";
+        std::cout << "╚════════════════════════╝\n";
         break;
     }
-    std::cout << "\n" << "Your first circle: " << "( " << circle1.center.x << ", " << circle1.center.y << ") " << circle1.r << "\n";
-    std::cout << "Your second circle: " << "( " << circle2.center.x << ", " << circle2.center.y << ") " << circle2.r << "\n";
     return 0;
 }
